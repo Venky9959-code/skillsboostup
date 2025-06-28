e9599bf07a32da7ac641b1d7ecbcf3f47b435b85
Banavathu Venkateswarao <venky995982@email.com>
2025-06-28 17:07:06 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")  # Renders your homepage
+    return render_template('index.html')  # Make sure you have templates/index.html
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
+    app.run(host='0.0.0.0', port=port)
