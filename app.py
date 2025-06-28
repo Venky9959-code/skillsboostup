2cad673fe1f26e68432be5b8ff7c5c03903af299
Banavathu Venkateswarao <venky995982@email.com>
2025-06-28 16:56:27 +0530

    Updated homepage to render HTML

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # Renders your homepage
+
+if __name__ == '__main__':
+    app.run(debug=True)
