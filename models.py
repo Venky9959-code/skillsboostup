3a4755dc48fb59a639633f505a306e1099f07f76
Banavathu Venkateswarao <venky995982@email.com>
2025-06-30 00:50:54 +0530

    UPDATED FINAL

--- models.py
+++ models.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(256))
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 
 class Course(db.Model):
