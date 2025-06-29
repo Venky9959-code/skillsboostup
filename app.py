4476a7db9a167becf3355c6fd9663ee2bb92d72c
Banavathu Venkateswarao <venky995982@email.com>
2025-06-29 15:44:38 +0530

    Add User and Course models

--- app.py
+++ app.py
@@ -34,3 +34,7 @@ def show_courses():
     return {
         "courses": [c.title for c in courses]
     }
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not found
+    app.run(host="0.0.0.0", port=port)
